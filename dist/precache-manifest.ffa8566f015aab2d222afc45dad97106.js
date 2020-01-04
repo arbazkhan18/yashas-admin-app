f1fa8395378ed1b12bd8481d9e0ac7871c6cdc01
arbaz khan18 <arbazkhan18@protonmail.com>
2020-01-04 16:45:45 +0530

    firebase deploy

---
+++ dist/precache-manifest.ffa8566f015aab2d222afc45dad97106.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8a70864e2d9635354c5b56dc1750e82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07359d25e3e04b12506d",
+    "url": "/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "3f9536dd97daca7226f0",
+    "url": "/static/js/2.e8bf1ad2.chunk.js"
+  },
+  {
+    "revision": "07359d25e3e04b12506d",
+    "url": "/static/js/main.00cfc18b.chunk.js"
+  },
+  {
+    "revision": "8582a421d5fcea5d17b9",
+    "url": "/static/js/runtime-main.168cb3e6.js"
+  }
+]);
